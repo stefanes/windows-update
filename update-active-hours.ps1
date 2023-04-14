aad6046d95f3ddc07f9f6df4395c06f11df243f1
Stefan Eskelid <stefanes@duck.com>
2023-04-14 14:02:44 +0200

    Improve date/time logic

--- update-active-hours.ps1
+++ update-active-hours.ps1
@@ -1,14 +1,12 @@
 #Requires -RunAsAdministrator
 
 $registryPath = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings"
-
-if (Test-Path -Path $registryPath) {
-    $currentHour = [DateTime]::Now.Hour
-    $activeHoursStart = $currentHour
-    $activeHoursEnd = $activeHoursStart + 12
-    if ($activeHoursEnd -gt 23) {
-        $activeHoursEnd = $activeHoursEnd - 24
-    }
-    Set-ItemProperty -Path $registryPath -Name ActiveHoursStart -Value $activeHoursStart
-    Set-ItemProperty -Path $registryPath -Name ActiveHoursEnd -Value $activeHoursEnd
+if (-Not (Test-Path -Path $registryPath)) {
+    New-Item -Path $registryPath -ItemType Directory -Force
 }
+
+$now = [DateTime]::Now
+$activeHoursStart = $now.Hour
+$activeHoursEnd = $now.AddHours(12).Hour
+Set-ItemProperty -Path $registryPath -Name ActiveHoursStart -Value $activeHoursStart
+Set-ItemProperty -Path $registryPath -Name ActiveHoursEnd -Value $activeHoursEnd
